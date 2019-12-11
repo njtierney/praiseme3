f5e7d260bf1078aa14cbbfa79ce809f3cb467256
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-12-11 15:33:14 +1100

    update punctuation argument documentation to note that it uses "!" by default. :rocket:

--- R/praise.R
+++ R/praise.R
@@ -2,8 +2,8 @@
 #'
 #' @description This is a function to deliver praise, super useful for anyone.
 #'
-#' @param name test string, the name of the person to praise.
-#' @param punctuation emphasis as a text input
+#' @param name text string, the name of the person to praise.
+#' @param punctuation text string, emphasis as a text input, default is "!".
 #'
 #' @return Text string with praise
 #'
