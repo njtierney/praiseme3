aaa4ef0524ea35b81689bfbcd7e9cd6aaf9e6ac3
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-12-11 15:43:31 +1100

    adds glue package instead of paste0

--- R/praise.R
+++ R/praise.R
@@ -12,5 +12,5 @@
 #' @examples
 #' praise(name = "Steph", punctuation = "!")
 praise <- function(name, punctuation = "!"){
-  paste0("You're the best, ", name, punctuation)
+  glue::glue("You're the best, {name}{punctuation}")
 }
